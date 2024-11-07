96364b7aeb1ca7271dfc0cee35b22bc80351dbd8
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-11-07 08:05:07 -0500

    chore: update font

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-roboto)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
