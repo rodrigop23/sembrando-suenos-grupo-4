8f39c513a5b8cda9b3be810ef0d0b7ac2d37a893
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-11-11 01:49:53 -0500

    refactor: rename GenericResponse to IGenericResponse and add IUser interface

--- apps/client/src/interface/generic.interface.ts
+++ apps/client/src/interface/generic.interface.ts
@@ -1,4 +1,4 @@
-export interface GenericResponse {
+export interface IGenericResponse {
   ok: boolean;
   message: string;
 }
---
+++ apps/client/src/interface/user.interface.ts
@@ -0,0 +1,5 @@
+export interface IUser {
+  id: number;
+  documentId: string;
+  email: string;
+}
